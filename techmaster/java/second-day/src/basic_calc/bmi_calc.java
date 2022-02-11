6081ae0d400f105fe64d357a37ef26d96f2f5c25
chinutt <77049971+nugort@users.noreply.github.com>
2022-02-12 06:19:55 +0700

    small update in code

--- techmaster/java/second-day/src/basic_calc/bmi_calc.java
+++ techmaster/java/second-day/src/basic_calc/bmi_calc.java
@@ -1,7 +1,7 @@
 package basic_calc;
 
 public class bmi_calc {
-        public static double BMI_value(float weigh, float height) {
+        public static double BMI_value(float height, float weigh) {
             float BMI_value = weigh / (height * height);
             return BMI_value;
         }
